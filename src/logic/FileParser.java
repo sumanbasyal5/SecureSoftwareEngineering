86ad846b0595cceec0d1e1f40631155a94cd86d7
sumanbasyal5 <basyal.suman866@gmail.com>
2017-10-06 20:14:35 -0500

    code for parsing of the file committed

--- src/logic/FileParser.java
+++ src/logic/FileParser.java
@@ -1,3 +1,5 @@
+package logic;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,29 +7,31 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SecureProgram {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("secure software engineering");
-		List<String> listString = new ArrayList<String>();
+public class FileParser {
+	public List<String> GetStringsFromFiles(String filename){
+		List<String> list= new ArrayList<String>();
 		try {
-			File file = new File("test.txt");
+			File file = new File(filename);
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
-				stringBuffer.append(line);
-				stringBuffer.append("\n");
+				//stringBuffer.append(line);
+				//stringBuffer.append("\n");
+				String [] array=line.split(" ");
+				for(int i=0;i<array.length;i++){
+					list.add(array[i]);
+				}
 			}
 			fileReader.close();
 			System.out.println("Contents of file:");
 			System.out.println(stringBuffer.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
+			return null;
 		}
-
+		return list;
+		
 	}
-
 }
